e7d2fcc535a49e2dab0470f1ab5c40d534fc7fe4
Nuno Martins <nuno.martins@externals.adidas.com>
2020-09-02 17:46:28 +0100

    missing stuff

---
+++ src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package guru.sfg.beer.inventory.service.web.mappers;
+
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.brewery.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+/**
+ * @author Nuno Martins
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
